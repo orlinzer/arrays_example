ffc54e2e99edded6c07523826e48f379932aa236
Or Linzer <orlinzer@gmail.com>
2022-04-24 16:56:50 +0300

    sod added

---
+++ sod.js
@@ -0,0 +1,8 @@
+
+let a = Math.floor(Math.random() * 9);
+let b = Math.floor(Math.random() * 9);
+let c = Math.floor(Math.random() * 9);
+
+console.log('sum:', a + b + c);
+console.log('product:', a * b * c);
+
